0f7d397b903c86a4aeb94606ead32ddc47592d4c
PhoneKhaingHein <phone@infinitalk.co.jp>
2024-01-30 00:28:26 +0630

    [Complete] Bogus CRUD

---
+++ DotNet8WebApi.BogusSample/Blog.cs
@@ -0,0 +1,18 @@
+﻿using Bogus;
+
+public class Blog
+{
+    public int BLogId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+}
+
+public class BlogFaker : Faker<Blog>
+{
+    public BlogFaker()
+    {
+        RuleFor(o => o.BLogId, f => f.Random.Number(1, 100));
+        RuleFor(o => o.Title, f => f.Lorem.Sentence(10));
+        RuleFor(o => o.Content, f => f.Lorem.Word());
+    }
+}
---
+++ DotNet8WebApi.BogusSample/Controllers/BlogController.cs
@@ -0,0 +1,61 @@
+﻿using Bogus;
+using DotNet8WebApi.BogusSample;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8WebApi.BogusSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BlogController : Controller
+    {
+        private readonly static List<Blog> _blogList = new List<Blog>();
+
+        [HttpPost(Name = "CreateBlog")]
+        public void CreateBlog(int count)
+        {
+            var blogFaker = new BlogFaker();
+
+            for (int i = 0; i < count; i++)
+            {
+                var blog = blogFaker.Generate();
+                _blogList.Add(blog);
+                Console.WriteLine($"Blog {i + 1}:");
+                Console.WriteLine($"BlogId: {blog.BLogId}, Title: {blog.Title}, Content: {blog.Content}\n");
+            }
+        }
+
+        [HttpGet(Name = "GetBlog")]
+        public IEnumerable<Blog> GetBlog()
+        {
+            return _blogList;
+        }
+
+        [HttpGet("{id}", Name = "GetBlogById")]
+        public Blog GetBlogById(int id)
+        {
+            return _blogList.FirstOrDefault(b => b.BLogId == id)!;
+        }
+
+        [HttpPut("{id}", Name = "UpdateBlog")]
+        public Blog UpdateBlog(int id, string title, string content)
+        {
+            var blog = _blogList.FirstOrDefault(b => b.BLogId == id);
+            if (blog != null)
+            {
+                blog.Title = title;
+                blog.Content = content;
+            }
+            return blog!;
+        }
+
+        [HttpDelete("{id}", Name = "DeleteBlog")]
+        public void DeleteBlog(int id)
+        {
+            var blog = _blogList.FirstOrDefault(b => b.BLogId == id);
+            if (blog != null)
+            {
+                _blogList.Remove(blog);
+            }
+        }
+    }
+}
